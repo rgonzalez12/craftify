f2e45c760a7571cd8c6c0c2c405587c0729975c7
Ray <raymondalexander.gonzalez@gmail.com>
2024-12-03 01:29:32 -0500

    added tailwind.css, updated package.json for stylesheet, updated viewlogic and created framework for react connection

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './craftify/**/*.html',
+    './craftify/**/*.js',
+    './craftify/**/*.jsx',
+    // Add other paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
