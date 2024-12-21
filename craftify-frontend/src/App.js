e279684dc9dc7ab597ab69300f40be49074255e1
Ray <raymondalexander.gonzalez@gmail.com>
2024-12-21 01:33:17 -0500

    updated app.js to add additional components and new pages, added additional dummy data for testing

--- craftify-frontend/src/App.js
+++ craftify-frontend/src/App.js
@@ -4,24 +4,29 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import MyItems from './components/MyItems';
 import ItemDetail from './components/ItemDetail';
+import CreateItem from './components/CreateItem';
+import EditItem from './components/EditItem';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Cart from './components/Cart';
 import Profile from './components/Profile';
+import EditProfile from './components/EditProfile';
 import DeleteUser from './components/DeleteUser';
 
 function App() {
   return (
     <Layout>
-      {/* Routes define which component to show for each path */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/items" element={<MyItems />} />
+        <Route path="/items/create" element={<CreateItem />} />
         <Route path="/items/:id" element={<ItemDetail />} />
+        <Route path="/items/:id/edit" element={<EditItem />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/profile/:id/edit" element={<EditProfile />} />
         <Route path="/delete_user/:id" element={<DeleteUser />} />
       </Routes>
     </Layout>
